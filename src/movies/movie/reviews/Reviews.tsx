fc3def88667b23a22bd5365af9b2855c25f23918
Daniel Splawski <daniel.splawski@codeconcept.pl>
2025-06-23 13:09:47 +0200

    Fixed memoized loadData function

--- src/movies/movie/reviews/Reviews.tsx
+++ src/movies/movie/reviews/Reviews.tsx
@@ -10,7 +10,7 @@ interface UseReviewsProps {
 
 function useReviews({ movieId }: UseReviewsProps) {
   const [reviews, setReviews] = useState<ReviewDto[]>([]);
-  const loadData = useCallback(() => () => {
+  const loadData = useCallback(() => {
     getMovieReviews(movieId).then((reviews) => {
       setReviews(reviews);
     });
