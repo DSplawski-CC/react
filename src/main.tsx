f1bc5e8479c9450e0b509d9f48aea9f7c780e65d
Daniel Splawski <daniel.splawski@codeconcept.pl>
2025-06-25 11:02:54 +0200

    set navigation route for login view

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import App from './App.tsx'
 import { BrowserRouter, Route, Routes } from 'react-router';
 import MoviesView from './movies/movies/MoviesView.tsx';
 import { MovieView } from './movies/movie/MovieView.tsx';
+import LoginView from './login/LoginView.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -12,6 +13,7 @@ createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path="/" element={<App />}>
           <Route path="/movies" element={<MoviesView />} />
+          <Route path="/sign-in" element={<LoginView />} />
           <Route path="/movie/:movieId" element={<MovieView />} />
         </Route>
       </Routes>
